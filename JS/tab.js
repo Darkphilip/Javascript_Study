e0dfb83d459faae062c25da4a6282b478aa29320
Darkphilip <yoonlucky0@gmail.com>
2022-02-18 14:09:43 +0900

    event bubbling study commit

--- JS/tab.js
+++ JS/tab.js
@@ -1,9 +1,19 @@
 
-for (let i = 0; i < $('.tab-button').length; i++) {
-  $('.tab-button').eq(i).click(function() {
-    $('.tab-button').removeClass('active');
-    $('.tab-content').removeClass('show');
-    $('.tab-button').eq(i).addClass('active');
-    $('.tab-content').eq(i).addClass('show');
-  })
+// for (let i = 0; i < $('.tab-button').length; i++) {
+//   $('.tab-button').eq(i).click(function() {
+//     openTab(i);
+//   })
+// }
+$('.list').data('작명', '값');
+
+$('.list').click(function(e) {
+    openTab(e.target.dataset.id)
+
+});
+
+function openTab(num) {
+  $('.tab-button').removeClass('active');
+  $('.tab-content').removeClass('show');
+  $('.tab-button').eq(num).addClass('active');
+  $('.tab-content').eq(num).addClass('show');
 }
